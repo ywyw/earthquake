964c7a01da568e39321b441ffe074390d038610b
ywyw <yuhongwang4@gmail.com>
2014-06-28 00:35:00 +0000

    more files

--- sstimes.py
+++ sstimes.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
 
-import ephem
+import ephem,datetime
 import sys 
 for line in sys.stdin:
          line = line.strip()
-         words = line.split(',')
-	 if (len(words) > 2) :
+         vals = line.split('\t')
+	 if (len(vals) > 2) :
 	         o=ephem.Observer()
-		 eqdate = words[0].split('T');
-		 o.date=eqdate[0]
-        	 o.lat=words[1]
-		 o.long=words[2]
-        	 s=ephem.Sun()
-        	 s.compute()
-        	 result=ephem.localtime(o.next_rising(s))
-        	 print '%s\t%s' % (result, line)
+		 eqdate = int(vals[2]);
+		 o.date=datetime.datetime.fromtimestamp(eqdate/1000)
+		 o.lat=vals[27]
+		 o.long=vals[26]
+        	 #print o.lat
+		 #print o.long
+		 s=ephem.Sun()
+        	 s.compute(o)
+		 m=ephem.Moon()
+		 m.compute(o.date)
+		 try:
+			risetime=ephem.date(o.next_rising(s))
+                        settime=ephem.date(o.next_setting(s))
+                        risediff = risetime-o.date
+                        setdiff = settime-o.date 
+			#print o.next_rising(s)
+		 except (ephem.AlwaysUpError, ephem.NeverUpError):
+			risediff=0
+			setdiff=0
+        		pass
+        	 print '%s\t%s\t%s\t%s\t%s' % (o.date, str(m.moon_phase), risediff, setdiff, line)
 	 else:
 		 print '%s\t%s' % ("",line)
          #print result
