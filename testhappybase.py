9b843a16e25d9037825c431fba05ed6e20563ef0
YW <ywyw@users.noreply.github.com>
2014-07-10 13:55:22 -0700

    Delete testhappybase.py

--- testhappybase.py
+++
@@ -1,7 +0,0 @@
-import happybase
-
-conn = happybase.Connection('localhost',port=8888)
-t = happybase.table('hbase_test')
-row = t.row('http://comcat.cr.usgs.gov/earthquakes/eventpage/atlas19600229234000')
-print conn.tables()
-#print row
